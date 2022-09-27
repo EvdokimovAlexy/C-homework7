fbdfdeea8ca0fb619a61a05bb43f6579a8d18c90
Prometheus <poligraphich@gmail.com>
2022-09-27 10:24:16 +0300

    add exam001

---
+++ example001_hw7/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+int rows = new Random().Next(2, 6);
+int columns = new Random().Next(2, 6);
+double[,] array = new double[rows, columns];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
