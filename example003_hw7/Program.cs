04e51a7f1f787bfb4c02f1e054ebc765766171e5
Prometheus <poligraphich@gmail.com>
2022-09-27 10:26:21 +0300

    add exam003

---
+++ example003_hw7/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int rows = new Random().Next(2, 6);
+int columns = new Random().Next(2, 6);
+int[,] array = new int[rows, columns];
+FillArrayRandomNumbers(array);
+PrintArray(array);
+Console.WriteLine("---------------------------");
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{sum / array.GetLength(0)} \t");
+}
+Console.WriteLine();
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    Console.WriteLine();
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
